ac572a4aa8a52e8742215b08c5e978abda1b8f77
DavaasurenOrgodol <orgodol.dawaasuren@gmail.com>
2023-11-20 11:38:46 -0600

    add changes

--- app.js
+++ app.js
@@ -29,44 +29,44 @@ const connection = mysql.createConnection({
     user: "root",
     port: 3306,
     password: "admin123",
-    database: "mysql"
+    database: "e_commerce"
 });
 
 // Connecting to database admin123
 //ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 //flush privileges;
 
-// connection.connect(function (err) {
-//     if (err) {
-//         console.log("Error in the connection")
-//         console.log(err)
-//     }
-//     else {
-//         console.log('Database Connected');
-//         connection.query('select * from users', //['admin'],
-//             function (err, result) {
-//                 if (err) {
-//                     console.log('Error executing the query - ${err}')
-//                 }
-//                 else {
-//                     console.log("Result: ", result);
-//                     setOutput(result);
-//                 }
+connection.connect(function (err) {
+    if (err) {
+        console.log("Error in the connection")
+        console.log(err)
+    }
+    else {
+        console.log('Database Connected');
+        connection.query('select * from users', //['admin'],
+            function (err, result) {
+                if (err) {
+                    console.log('Error executing the query - ${err}')
+                }
+                else {
+                    //console.log("Result: ", result);
+                    setOutput(result);
+                }
 
-//             });
-//             connection.query('select * from product', //['admin'],
-//             function (err, result) {
-//                 if (err) {
-//                     console.log('Error executing the query - ${err}')
-//                 }
-//                 else {
-//                     console.log("Result: ", result);
-//                     setProduct(result);
-//                 }
+            });
+            connection.query('select * from product', //['admin'],
+            function (err, result) {
+                if (err) {
+                    console.log('Error executing the query - ${err}')
+                }
+                else {
+                   // console.log("Result: ", result);
+                    setProduct(result);
+                }
 
-//             });
-//     }
-// });
+            });
+    }
+});
 
 app.get('/', function (req, res, next) {
     res.cookie('email', '');
@@ -107,3 +107,7 @@ app.use(cartRouter);
 //         res.sendFile(path.join(__dirname, "views", "login.html"));
 //     }
 // });
+
+app.get('/ajaxProducts',function(req,res){
+    res.send({products})
+})
